5c0875f53ec19961e7e78fd1bec6b5f7c4a9a9e7
Stormyisland <jacqueslavoie2015@gmail.com>
2024-06-05 09:28:07 -0700

    im commited

---
+++ main.py
@@ -0,0 +1,15 @@
+import random 
+
+
+
+
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+numbers = "1234567890"
+symbols = "~!@#$%^&*()_+=`?><{][]}` "
+
+string = uppercase_letters + lowercase_letters +  numbers
+length = 18 
+password = "".join(random.sample(string,length ))
+
+print(" ""\n Your new password is:" + password + " ya rich ya know. \n")
